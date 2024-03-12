e76182c941120b4696a988e1527e12c5155c2596
Hardin-J <105577218+Hardin-J@users.noreply.github.com>
2024-03-12 09:41:50 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,7 @@
 public class Sample{
   public static void main(String []arg){
+    //it is a sample code
     System.out.println("Hello World");  
+    System.out.println("Welcome you all");
   }
 }
