44479fe96eb2121bd910dccd46cec9b01dc05271
Hardin-J <105577218+Hardin-J@users.noreply.github.com>
2024-03-12 09:57:16 +0530

    Update Sample.java in branch 2

--- Sample.java
+++ Sample.java
@@ -3,5 +3,6 @@ public class Sample{
     //it is a sample code
     System.out.println("Hello World");  
     System.out.println("Welcome you all");
+    System.out.println("edited in branch 2");
   }
 }
